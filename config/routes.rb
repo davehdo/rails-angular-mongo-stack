b8d76820e47ccadb440f133134a1841822644455
David Do <davehdo@gmail.com>
2015-12-17 22:49:36 -0500

    created patients controller which returns json

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  resources :patients, only: [:index, :create, :update, :destroy], defaults: {format: :json}
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
